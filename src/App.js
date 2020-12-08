b89c404373fcbc94a03f2f553612986a404d5c24
Semilore Idowu <semijacks27@gmail.com>
2020-12-08 07:52:07 +0100

    scrollToTop component not performing any function, so i removed it

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import HomePage from "./screens/HomePage";
 import PortfolioItem from "./screens/PortfolioItem";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 function App() {
   return (
     <Router>
-      <ScrollToTop>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/portfolio-item" component={PortfolioItem} />
-        </Switch>
-      </ScrollToTop>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/portfolio-item" component={PortfolioItem} />
+      </Switch>
     </Router>
   );
 }
